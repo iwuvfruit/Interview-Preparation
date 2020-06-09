0d41216e3afc70ffdf91b15aedc147982385fa4d
iwuvfruit <iwuvfruit@gmail.com>
2020-06-08 23:03:11 -0600

    q4 added

--- Interview Cake/src/HashingQ4.java
+++ Interview Cake/src/HashingQ4.java
@@ -1,4 +1,32 @@
+import java.util.Arrays;
 
 public class HashingQ4 {
+	public static void main(String[] args) {
+		int[] unsortedScores = {37, 89, 41, 65, 91, 53};
+		final int HIGHEST_POSSIBLE_SCORE = 100;
+		int[] x = sortScores(unsortedScores,HIGHEST_POSSIBLE_SCORE);
+		System.out.println(Arrays.toString(x));
+	}
+			
+	public static int[] sortScores(int[] unorderedScores, int highestPossibleScore) {
+		
+		int[] scoreCounts = new int[highestPossibleScore+1];
+		for(int score: unorderedScores) {
+			scoreCounts[score]++;
+		}
+		
+		int[] sortedScores = new int[unorderedScores.length];
+		int currentSortedIndex = 0;
+		for(int score = highestPossibleScore; score >= 0; score--) {
+			int count = scoreCounts[score];
+			
+			for(int occurence = 0; occurence <count; occurence++) {
+				sortedScores[currentSortedIndex] = score;
+				currentSortedIndex++;
+			}
+		}
+		return sortedScores;
+		
+	}
 
 }
